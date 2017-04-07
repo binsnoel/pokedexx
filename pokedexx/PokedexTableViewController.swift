e7f449e13974dbad649f858e2b1bc40d910ffc30
binsnoel <bins@binsmba.local>
2017-04-08 03:03:52 +0800

    added search bar not yet implemented

--- pokedexx/PokedexTableViewController.swift
+++ pokedexx/PokedexTableViewController.swift
@@ -10,21 +10,42 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
+extension PokedexTableViewController: UISearchResultsUpdating {
+    @available(iOS 8.0, *)
+    public func updateSearchResults(for searchController: UISearchController) {
+//        code
+    }
+
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+//        filterContentForSearchText(searchController.searchBar.text!)
+    }
+}
+
 
 class PokedexTableViewController: UITableViewController {
     
+    let searchController = UISearchController(searchResultsController: nil)
     var jsonPokedex = [[String : AnyObject]]()
     var arrayPokedex = [Int32 : String]()
+    var filteredPokedex = [Int32: String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchPokedex()
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
     }
+    
+//    func filterContentForSearchText(searchText: String, scope: String = "All") {
+//        filteredPokedex = arrayPokedex.filter { pokemon in
+//            return pokemon[.lowercaseString.containsString(searchText.lowercaseString)
+//        }
+//        
+//        tableView.reloadData()
+//    }
     
     func fetchPokedex() {
         let uri = Constants.baseUri + Constants.pokedexUri + "2/"
@@ -99,50 +120,4 @@ class PokedexTableViewController: UITableViewController {
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
